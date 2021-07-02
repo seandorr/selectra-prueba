c83ee4d1c2d42b5c0f2bd1d55631537b03db9956
Sean Dorr <seanmichael.dorr@gmail.com>
2021-07-02 17:28:16 +0200

    CREATED: contact card component to display info

---
+++ src/components/ContactCard/ContactCard.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Idcard from "@ant-design/icons/IdcardOutlined";
+import ArrowBtn from "@ant-design/icons/RightCircleTwoTone";
+import "./contact-card.scss";
+
+const ContactCard = ({ contact }) => {
+  const { name, email, id } = contact;
+
+  const [hoveringOverIcon, setHoveringOverIcon] = useState(false);
+
+  const handleOnMouseEnter = (id) => {
+    setHoveringOverIcon(id);
+  };
+
+  return (
+    <div
+      className={`contact-card ${
+        hoveringOverIcon === id ? "active-hover" : ""
+      }`}
+    >
+      <div className="card-icon center">
+        <Idcard />
+      </div>
+      <div className="card-info__name center">
+        <h1 className="card-info__item">{name}</h1>
+      </div>
+      <div className="card-info__email center">
+        <p className="card-info__item">{email}</p>
+      </div>
+      <div
+        className="card-button center"
+        onMouseEnter={() => handleOnMouseEnter(id)}
+        onMouseLeave={() => setHoveringOverIcon(false)}
+      >
+        <ArrowBtn />
+      </div>
+    </div>
+  );
+};
+
+ContactCard.propTypes = {
+  contact: PropTypes.object.isRequired,
+};
+
+export default ContactCard;
