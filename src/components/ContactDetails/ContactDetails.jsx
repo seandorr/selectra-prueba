3e11d8871a8d78f8fbe05a175878ec5d436157a1
Sean Dorr <seanmichael.dorr@gmail.com>
2021-07-04 16:16:37 +0200

    FEATURE: Created contact details component

---
+++ src/components/ContactDetails/ContactDetails.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./contact-details.scss";
+import ContactFormRow from "./components/ContactFormRow";
+
+const ContactDetails = ({ contact }) => {
+  const { name, email, username, address } = contact;
+  const { city, street, suite, zipcode } = address;
+
+  return (
+    <div className="contact-details__container">
+      <div className="flex">
+        <h1>{name}</h1>
+        <a className="button" href="/">
+          Back
+        </a>
+      </div>
+      <form>
+        <ContactFormRow
+          labelText="Full Name"
+          inputName="name"
+          defaultInputValue={name}
+        />
+        <ContactFormRow
+          labelText="Email"
+          inputName="email"
+          defaultInputValue={email}
+        />
+        <ContactFormRow
+          labelText="Username"
+          inputName="username"
+          defaultInputValue={username}
+        />
+        <ContactFormRow
+          labelText="Address"
+          inputName="address"
+          defaultInputValue={`${street}, ${suite}, ${city}, ${zipcode}`}
+        />
+      </form>
+    </div>
+  );
+};
+
+ContactDetails.propTypes = {
+  contact: PropTypes.object.isRequired,
+};
+
+export default ContactDetails;
---
+++ src/components/ContactDetails/components/ContactFormRow.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ContactFormRow = ({ labelText, inputName, defaultInputValue }) => {
+  return (
+    <div className="form-row">
+      <label>{labelText}:</label>
+      <input name={inputName} type="text" defaultValue={defaultInputValue} />
+    </div>
+  );
+};
+
+ContactFormRow.propTypes = {
+  labelText: PropTypes.string,
+  inputName: PropTypes.string,
+  defaultInputValue: PropTypes.string,
+};
+
+ContactFormRow.defaultProps = {
+  labelText: undefined,
+  inputName: undefined,
+  defaultInputValue: undefined,
+};
+
+export default ContactFormRow;
